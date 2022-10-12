412b044775320063ed196a9bcd7b246bd2cbe0d6
temitopeAdeyemo <temitopejulius9@gmail.com>
2022-10-12 22:30:08 +0100

    chore:Refractored auth Service

--- __tests__/auth.test.js
+++ __tests__/auth.test.js
@@ -1,12 +1,14 @@
 const request = require("supertest");
-const app = require("../app");
+const App = require("../src/shared/app").default;
+const app = new App().getApp();
 
 describe("Auth API", () => {
   it("should be able to register new user", async () => {
-    const response = await request(app).post("/api/v1/auth/register").send({
-      name: "test",
+    const response = await request(app).post("/v1/auth/register").send({
+      full_name: "test",
       email: "test",
       password: "test",
+      phoneNumber: "08134009444"
     });
 
     expect(response.statusCode).toBe(201);
@@ -14,7 +16,7 @@ describe("Auth API", () => {
   }).timeout(10000);
 
   it("should be able to login a user", async () => {
-    const response = await request(app).post("/api/v1/auth/login").send({
+    const response = await request(app).post("/v1/auth/login").send({
       email: "test",
       password: "test",
     });
--- __tests__/user-account.test.js
+++ __tests__/user-account.test.js
@@ -1,31 +1,9 @@
 const request = require("supertest");
-const app = require("../app");
+const App = require("../src/shared/app").default;
+const app = new App().getApp();
 
 describe("Account API", () => {
-  // it("should create a user account", async () => {
-  //   const response = await request(app).post("/api/v1/users/account").send({
-  //     type: "test",
-  //     number: "test",
-  //     balance: "test",
-  //     user_id: "test",
-  //   });
-
-  //   expect(response.statusCode).toBe(201);
-  //   expect(response.body.message).toBe("User Bank Account successfully");
-  // }).timeout(10000);
-
-  // it("authenticated user should be able to get all their bank account", async () => {
-  //   const response = await request(app)
-  //     .get("/api/v1/users/account")
-  //     .set("Authorization", "Bearer " + token);
-
-  //   expect(response.statusCode).toBe(200);
-  //   expect(response.body.message).toBe(
-  //     "User bank account retrieved successfully"
-  //   );
-  // });
-
-  it("authenticated user should be able to fund their bank account", async () => {
+  it("should be able to fund bank account", async () => {
     const response = await request(app)
       .post("/v1/account/fund")
       .set("Authorization", "Bearer " + token)
@@ -39,16 +17,27 @@ describe("Account API", () => {
 
   it("authenticated user should be able to transfer funds to another user", async () => {
     const response = await request(app)
-      .post("/api/v1/users/account/1/transfer")
+      .post("/v1/account/transfer-fund")
       .set("Authorization", "Bearer " + token)
       .send({
-        amount: 100,
-        to: 2,
+        amount: "100",
+        accountNumber: "08134844747",
+        pin: "1234",
       });
 
     expect(response.statusCode).toBe(200);
-    expect(response.body.message).toBe(
-      "User bank account transferred successfully"
-    );
+    expect(response.body.message).toBe("Fund transferred successfully");
+  }).timeout(10000);
+
+  it("authenticated user should be able to withdraw funds from their account", async () => {
+    const response = await request(app)
+      .post("/v1/account/withdraw-fund")
+      .set("Authorization", "Bearer " + token)
+      .send({
+        amount: "100",
+      });
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.message).toBe("Fund withdrawn successfully");
   }).timeout(10000);
 });
